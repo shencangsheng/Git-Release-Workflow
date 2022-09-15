9e63ad7cb623be61bf39e3adf6067cb32b2adaf3
shencangsheng <shencangsheng@126.com>
2022-09-15 16:38:49 +0800

    feat: CI/CD generic

--- post-gitlab-release-generic-14.sh
+++ post-gitlab-release-generic-14.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl --request POST "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/package/${CI_COMMIT_TAG}/${1}" \
+curl --request POST "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/generic/release/${CI_COMMIT_TAG}/${1}" \
     --header "JOB-TOKEN: ${CI_JOB_TOKEN}" \
     --upload-file ${2}
